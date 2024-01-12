594c090e7941f1e4b9a10145bfc39b7725a6cf01
wukai <is.wukai@outlook.com>
2024-01-13 00:47:00 +0800

    添加点击事件

--- popup.js
+++ popup.js
@@ -4,3 +4,10 @@ window.onload = async function () {
   const bookmarkTree = await chrome.bookmarks.getTree();
   console.log("tree: ", bookmarkTree);
 };
+
+const item = document.querySelector(".item");
+item.addEventListener("click", () => {
+  console.log("click", item.firstElementChild);
+  const icon = item.firstElementChild;
+  icon.style.transform = "rotate(90deg)";
+});
