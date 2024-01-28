66f2ba3b2dea89abadfafd6acd9ecc1387fbdb94
wukai <is.wukai@outlook.com>
2024-01-29 01:18:11 +0800

    样式优化

--- popup.js
+++ popup.js
@@ -8,6 +8,12 @@ const searchInput = document.querySelector(".search-input");
 let searchResultElements = [];
 let curFocusIndex = 0;
 
+window.onload = async function () {
+  const bookmarkBarTree = await chrome.bookmarks.getSubTree("1"); // 书签栏
+  const subTree = bookmarkBarTree[0].children;
+  listBookmarks(bookmarksBox, subTree);
+};
+
 // on search input value change
 searchInput.oninput = async () => {
   if (!searchInput.value) {
@@ -23,7 +29,7 @@ searchInput.oninput = async () => {
   const resultBookmarks = await chrome.bookmarks.search(searchInput.value);
   if (resultBookmarks.length > 0) {
     resultBookmarks.forEach((bookmark) => {
-      const item = generateItem(bookmark, true);
+      const item = generateItemV2(bookmark, 0, true);
       searchResultBox.append(item);
     });
 
@@ -54,13 +60,7 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-window.onload = async function () {
-  const bookmarkBarTree = await chrome.bookmarks.getSubTree("1"); // 书签栏
-  const subTree = bookmarkBarTree[0].children;
-  listBookmarks(bookmarksBox, subTree);
-};
-
-function listBookmarks(container, bookmarks) {
+function listBookmarks(container, bookmarks, level = 0) {
   if (bookmarks.length === 0) {
     const noDataBox = createElement("div", "no-data", "请添加书签");
     container.append(noDataBox);
@@ -68,48 +68,53 @@ function listBookmarks(container, bookmarks) {
   }
 
   bookmarks.forEach((node) => {
-    const item = generateItem(node);
+    const item = generateItemV2(node, level);
     container.append(item);
   });
 }
 
-function generateItem(bookmarkNode, focusable = false) {
+function generateItemV2(bookmarkNode, level, focusable = false) {
   const item = createElement("div", "item");
+  const children = [];
+  item.setAttribute("level", level);
+
+  // 搜索结果回车触发
   if (focusable) {
     item.setAttribute("tabindex", "-1");
     item.onfocus = () => openNewTab(bookmarkNode.url);
   }
 
+  // 添加缩进
+  for (let i = 0; i < level; i++) {
+    children.push(createElement("div", "item-tab"));
+  }
+
   const isFolder = bookmarkNode.children;
   if (isFolder) {
-    const icon = generateIcon();
-    item.append(icon);
+    const toggleIcon = generateIcon();
+    children.push(toggleIcon);
+    item.addEventListener("click", () => {
+      toggleIcon.classList.toggle("expand");
+      if (item.nextElementSibling?.classList.contains("next-level-container")) {
+        // 已经渲染过下一级
+        item.nextElementSibling.classList.toggle("hidden");
+      } else {
+        const childrenContainer = createElement("div", "next-level-container");
+        item.after(childrenContainer);
+        listBookmarks(childrenContainer, bookmarkNode.children, level + 1);
+      }
+    });
   } else {
     const favicon = createElement("img", "favicon");
     favicon.src = getFaviconUrl(bookmarkNode.url);
-    item.append(favicon);
+    children.push(favicon);
+    item.addEventListener("click", () => openNewTab(bookmarkNode.url));
   }
 
-  const titleSpan = createElement("span", "title", bookmarkNode.title);
-  item.append(titleSpan);
+  const title = createElement("div", "title", bookmarkNode.title);
+  children.push(title);
 
-  item.addEventListener("click", async () => {
-    if (isFolder) {
-      const icon = item.firstElementChild;
-      icon.classList.toggle("expand");
-
-      if (item.nextElementSibling?.classList.contains("left-padding")) {
-        // already rendered item's children
-        item.nextElementSibling.classList.toggle("hidden");
-      } else {
-        const childrenContainer = createElement("div", "left-padding");
-        item.after(childrenContainer);
-        listBookmarks(childrenContainer, bookmarkNode.children);
-      }
-    } else {
-      openNewTab(bookmarkNode.url);
-    }
-  });
+  item.append(...children);
 
   return item;
 }
@@ -119,8 +124,8 @@ function openNewTab(url) {
 }
 
 function generateIcon() {
-  const icon = createElement("span", "icon");
-  icon.innerHTML = `<svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+  const toggleIcon = createElement("div", "toggle-icon");
+  toggleIcon.innerHTML = `<svg width="24" height="24" fill="none" viewBox="0 0 24 24">
                       <path
                         stroke="currentColor"
                         stroke-linecap="round"
@@ -130,7 +135,7 @@ function generateIcon() {
                       />
                     </svg>`;
 
-  return icon;
+  return toggleIcon;
 }
 
 function removeAllChildrenEl(parent) {
